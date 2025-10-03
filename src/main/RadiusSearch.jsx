6188b1817d0ed43bdde97da910fbf6b78c2d2a3f
Simon Herkenhoff <13447418+sherkenhoff@users.noreply.github.com>
2025-10-02 23:48:56 -0400

    Implement radius search functionality with drag-and-drop support in MainMap component

--- src/main/RadiusSearch.jsx
+++ src/main/RadiusSearch.jsx
@@ -152,6 +152,7 @@ const RadiusSearch = ({ onResultsFound }) => {
         ref={searchRef}
         onClick={handleSearchPanelOpen}
         title={t('Radius Search')}
+        onDragStart={(e) => e.dataTransfer.setData('application/traccar-radius-search', 'true')}
       >
         <SearchIcon />
       </IconButton>
